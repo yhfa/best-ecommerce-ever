6912c1f63ee0867e22a94dc4c18bf2d17282008a
yhf <youssifhany75@gmail.com>
2022-09-22 12:10:43 +0200

    add: product controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,103 @@
+import { Response, Request, NextFunction } from 'express';
+
+import { Methods } from '../models/interfaces';
+import { ProductService } from '../services/product.service';
+import Controller from '../models/controller';
 import { checkAuth } from '../middlewares/check-auth';
+import { HttpError } from './../models/errors';
+
+class ProductController extends Controller {
+  path = '/products';
+
+  productService: ProductService;
+
+  constructor() {
+    super();
+    this.productService = new ProductService();
+  }
+
+  handleGetProducts = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      console.log(this.productService);
+      const products = await this.productService.getProducts();
+
+      res.status(200).json({ status: 'success', data: { products } });
+    } catch (error) {
+      console.error('Error 💥', error);
+      return next(new HttpError(404, error.message));
+    }
+  };
+
+  handleCreateProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const product = req.body;
+
+      const savedProduct = await this.productService.createProduct(
+        product,
+        req['userId']
+      );
+
+      res
+        .status(201)
+        .json({ status: 'success', data: { product: savedProduct } });
+    } catch (error) {
+      console.error('Error 💥', error);
+      return next(new HttpError(403, error.message));
+    }
+  };
+
+  handleUpdateProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const product = req.body;
+      const productId = req.params.id;
+
+      const updatedProduct = await this.productService.updateProduct(
+        productId,
+        product,
+        req['userId']
+      );
+
+      res
+        .status(201)
+        .json({ status: 'success', data: { product: updatedProduct } });
+    } catch (error) {
+      console.error('Error 💥', error);
+      return next(new HttpError(403, error.message));
+    }
+  };
+
+  routes = [
+    {
+      path: '/', // Will become /products/
+      method: Methods.GET,
+      handler: this.handleGetProducts,
       localMiddleware: [checkAuth],
+    },
+    {
+      path: '/', // Will become /products/
+      method: Methods.POST,
+      handler: this.handleCreateProduct,
       localMiddleware: [checkAuth],
+    },
+    {
+      path: '/:id', // Will become /products/
+      method: Methods.PUT,
+      handler: this.handleUpdateProduct,
       localMiddleware: [checkAuth],
+    },
+  ];
+}
+
+export default ProductController;
